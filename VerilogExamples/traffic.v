6f33970ce63af52114b20c81d55c83ae35fc5eae
Petri V <petri.valisuo@uva.fi>
2020-02-18 00:46:37 +0200

    Updated traffic lights example

--- VerilogExamples/traffic.v
+++ VerilogExamples/traffic.v
@@ -9,8 +9,29 @@ module traffic (output logic start_timer,
     if (~n_reset)
       present_state <= G;
     else
-      present state <= next_state;
+      present_state <= next_state;
   end
 
-  
+  // Implement this always as combinatorics
+  always_comb begin
+    start_timer = '0;
+    minor_green = '0;
+    major_green = '0;
+    next_state = present_state;
+    case (present_state)
+      G: begin
+        major_green ='1;
+        if (car)
+          begin
+            start_timer = '1;
+            next_state = R;
+          end
+        end
+      R: begin
+        minor_green = 1'b1;
+        if (timed)
+          next_state = G;
+        end
+    endcase
+  end
 endmodule
